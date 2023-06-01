3e08d27972b12b9a22cfd40fed905be319e5a444
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-06-01 09:54:07 -0400

    updating readme file

--- Bipartite Graph.java
+++ Bipartite Graph.java
@@ -1,55 +1,88 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         
-        int color[]=new int[graph.length];
         ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        // for(int i=0;i<adj.size();i++)
+        // {
+        //     adj.get(i)=new ArrayList<Integer>();
+        // }
         for(int i=0;i<graph.length;i++)
         {
-            ArrayList<Integer>al=new ArrayList<>();
+            ArrayList<Integer>al=new ArrayList();
             for(int j=0;j<graph[i].length;j++)
             {
                 al.add(graph[i][j]);
             }
             adj.add(al);
-        } 
-        for(int i=0;i<graph.length;i++)
+        }
+         int color[]=new int[graph.length];
+        // int visited[]=new int[graph.length];
+        for(int i=0;i<color.length;i++)
         {
             color[i]=-1;
         }
-        for(int i=0;i<graph.length;i++)
+        // for(int i=0;i<graph.length;i++)
+        // {
+        //     if(color[i]==-1)
+        //     {
+        //         if(BFS(adj,0,i,color)==false)
+        //         {
+        //             return false;
+        //         }
+        //     }
+        // }
+
+          for(int i=0;i<graph.length;i++)
         {
             if(color[i]==-1)
             {
-                if(check(i,graph.length,adj,color)==false)
+                if(DFS(adj,-1,i,color)==false)
                 {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+    public boolean BFS(ArrayList<ArrayList<Integer>>adj,int col,int start,int color[])
+    {
+       Queue<Integer> q = new LinkedList<Integer>();
+	    q.add(start); 
+	    color[start] = 0; 
+	    while(!q.isEmpty()) {
+	        int node = q.peek();
+	        q.remove(); 
+	        
+	        for(int it : adj.get(node)) {
+	            if(color[it] == -1) {
+	                
+	                color[it] = 1 - color[node]; 
+	                q.add(it); 
+	            }
+	            else if(color[it] == color[node]) {
+	                return false; 
+	            }
+	        }
+	    }
+	    return true; 
+
+    }
+    public boolean DFS(ArrayList<ArrayList<Integer>>adj,int col,int start,int color[])
+    {
+        color[start]=col;
+        for(int it:adj.get(start))
+        {
+            if(color[it]==-1)
+            {
+                if(DFS(adj,1-col,it,color)==false)
+                return false;
+            }
+            else if(color[it]==col)
+            {
+                return false;
             }
         }
         return true;
 
-    }
-    public boolean check(int i,int V,ArrayList<ArrayList<Integer>> adj,int color[])
-    {
-        Queue<Integer>q=new LinkedList<>();
-        q.add(i);
-        color[i]=0;
-        while(!q.isEmpty())
-        {
-            int node=q.remove();
-            for(int it:adj.get(node))
-            {
-                if(color[it]==-1)
-                {
-                    color[it]=1-color[node];
-                    q.add(it);
-                }
-                else if(color[node]==color[it])
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
--- README.md
+++ README.md
@@ -1,19 +1,79 @@
 # Graph
 
 
+<h2>Graph Traversals</h2>
+
 
 <table>
   <tr>
-    <th>Questions</th>
-    <th>Url</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-
-  </tr>
-  <tr>
     <td > BFS Traversal</td>
-    <td><button href="https://github.com/reeteshkesarwani/Graph/blob/main/BFS%20traversal.java">BFS</button></td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/BFS%20traversal.java"> BFS Traversal</a></td>
+  </tr>
+    <tr>
+    <td > DFS Traversal</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/DFS%20traversal.java"> DFS Traversal</a></td>
   </tr>
 </table>
+
+<h2>Questions On Traversals</h2>
+<table>
+  <tr>
+    <td > Number of Provinces</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Number%20of%20Provinces.java"> Number of Provinces</a></td>
+  </tr>
+    <tr>
+    <td > Number of Island</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Number%20of%20Island.java"> Number of Island</a></td>
+  </tr>
+      <tr>
+    <td > Flood Fill</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/FloodFill.java"> Flood Fill</a></td>
+  </tr>
+  <tr>
+    <td > Rotten Oranges</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/RottenTomatoes.java"> Rotten Oranges</a></td>
+  </tr>
+    <tr>
+    <td > Detect Cycle Using BFS</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Detect%20Directed%20Graph%20using%20BFS.java"> Detect Cycle Using BFS</a></td>
+  </tr>
+     <tr>
+    <td > Detect Cycle Using DFS</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Detect%20Direction%20Graph%20using%20DFS.java"> Detect Cycle Using DFS</a></td>
+  </tr>
+
+   <tr>
+    <td > Distance of nearest cell having 1</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Distance%20of%20nearest%20cell%20is%201.java"> Distance of nearest cell having 1</a></td>
+  </tr>
+
+  <tr>
+    <td > Replace O's with X's</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Replace%20O's%20with%20X's.java"> Replace O's with X's</a></td>
+  </tr>
+
+  <tr>
+    <td > Number of Enclaves</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Number%20of%20Enclaves.java"> Replace O's with X's</a></td>
+  </tr>
+
+  <tr>
+    <td > Number of Distinct Island</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Number%20of%20Distinct%20Islands.java"> Number of Distinct Island</a></td>
+  </tr>
+
+</table>
+
+<h2>Bipartite Graph</h2>
+
+<table>
+  <tr>
+    <td > Bipartite Graph using BFS</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Bipartite%20Graph.java"> Bipartite Graph using BFS</a></td>
+  </tr>
+    <tr>
+    <td > Bipartite Graph using DFS</td>
+    <td> <a href="https://github.com/reeteshkesarwani/Graph/blob/main/Bipartite%20Graph.java"> Bipartite Graph using DFS</a></td>
+  </tr>
+</table>
+
